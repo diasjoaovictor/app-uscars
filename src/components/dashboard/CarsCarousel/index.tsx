fff1525fdf9ac6ebb3641dc1a506ddb39359a8f2
neres3518 <davidneres4554@gmail.com>
2021-12-30 19:44:51 -0300

    setting responsive mode

--- src/components/dashboard/CarsCarousel/index.tsx
+++ src/components/dashboard/CarsCarousel/index.tsx
@@ -37,7 +37,7 @@ export function CarsCarousel(){
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 4
+      items: 2
     },
     mobile: {
       breakpoint: { max: 400, min: 20 },
