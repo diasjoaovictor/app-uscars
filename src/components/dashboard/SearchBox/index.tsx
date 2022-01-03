836c65ec054ad98ab4328f5fd092e377f23b94aa
neres3518 <davidneres4554@gmail.com>
2022-01-03 14:13:46 -0300

    SearchBox - Add Icon

--- src/components/dashboard/SearchBox/index.tsx
+++ src/components/dashboard/SearchBox/index.tsx
@@ -1,4 +1,5 @@
-import { Flex, Input, Icon } from '@chakra-ui/react';
+import { Flex, Input, Icon, InputGroup, InputRightElement} from '@chakra-ui/react';
+import {  SearchIcon } from '@chakra-ui/icons';
 import { RiSearchLine } from 'react-icons/ri';
 
 export function SearchBox(){
@@ -8,29 +9,24 @@ export function SearchBox(){
       justifyContent="center"
     >
       <Flex
-      w="80%"
-      justifyContent="space-between"
-      as="label"
-      bgColor="gray.200"
-      position="relative"
+        w="80%"
+        justifyContent="space-between"
+        as="label"
+        bgColor="gray.200"
+        position="relative"
       >
-        <Input
-          bgColor="gray.200"
-          w="95%"
-          borderRadius={0}
-          border="none"
-          placeholder="Search in US Cars Shop"
-          color="black.900"
-        />
-        <Icon
-          as={RiSearchLine}
-          fontSize={20}
-          mt={2}
-          marginRight={[10, 15, 17]}
-        />
-
+        <InputGroup>
+          <Input
+            bgColor="gray.200"
+            w="100%"
+            borderRadius={0}
+            border="none"
+            placeholder="Search in US Cars Shop"
+            color="black.900"
+          />
+          <InputRightElement children={<SearchIcon h="40px"/>}/>
+        </InputGroup>
       </Flex>
-
     </Flex>
   )
 }
